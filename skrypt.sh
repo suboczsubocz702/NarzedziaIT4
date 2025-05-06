3417e28b73771e3a6e1b8a7c95be38ae01dbf2fb
suboczsubocz702 <suboczmateusz08@gmail.com>
2025-05-06 15:25:52 +0200

    Update skrypt.sh

--- skrypt.sh
+++ skrypt.sh
@@ -44,3 +44,17 @@ case "$1" in
         exit 1
         ;;
 esac
+
+if [[ $1 == "--logs" ]]; then
+    count=100
+    if [[ $2 =~ ^[0-9]+$ ]]; then
+        count=$2
+    fi
+
+    for i in $(seq 1 $count); do
+        filename="log${i}.txt"
+        echo "Nazwa pliku: $filename" > "$filename"
+        echo "Skrypt: $0" >> "$filename"
+        echo "Data: $(date)" >> "$filename"
+    done
+fi
